2f14fb1ace1cc0a2b7a3a63d833efa57150fdfb7
JPST-Developer <jpst_2001@hotmail.com>
2022-03-13 05:31:09 -0300

    Tela de autentificação terminada

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,8 @@ import illustrationImg from "../assets/images/illustration.svg"
 import logoImg from "../assets/images/logo.svg"
 import googleIconImg from "../assets/images/google-icon.svg"
 
+import { Button } from "../components/Button"
+
 import "../styles/auth.scss"
 
 export  function Home(){
@@ -15,18 +17,17 @@ export  function Home(){
       <main>
         <div className="main-content">
           <img src={logoImg} alt=""  />
-          <button>
+          <button className="create-room">
             <img src={googleIconImg} alt="Logo do google" />
             Crie sua sala com o Google
           </button>
         </div>
-        <div>
-          Ou entre em uma salas
-          <form>
-            <input type="text" placeholder="Digite o código da sala" />
-            <button type="submit">Entrar na sala</button>
-          </form>
-        </div>
+        <div className="separator">Ou entre em uma salas</div>
+        <form>
+          <input type="text" placeholder="Digite o código da sala" />
+          <Button type="submit">Entrar na sala</Button>
+        </form>
+        
       </main>
     </div>
   )
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,13 +4,13 @@ import "firebase/compat/auth"
 import "firebase/compat/database"
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
+  apiKey: "AIzaSyDc4ik22-5BEjlX16ixveaN7h69Ow-20LE",
+  authDomain: "letmeask-b57ad.firebaseapp.com",
+  databaseURL: "https://letmeask-b57ad-default-rtdb.firebaseio.com",
+  projectId: "letmeask-b57ad",
+  storageBucket: "letmeask-b57ad.appspot.com",
+  messagingSenderId: "9349160992",
+  appId: "1:9349160992:web:34147bb9e62bba092fdc45"
 };
 
 firebase.initializeApp(firebaseConfig)
